7d5668c2b2c6b1058c8d73239cfd49e1696615fa
Tanvi-78 <dalvitanvi2008@gmail.com>
2026-06-15 10:33:26 +0530

    Day 2:Added order by protocols

--- Genesis_Query_Protocol.sql
+++ Genesis_Query_Protocol.sql
@@ -13,3 +13,26 @@ SELECT * FROM Students WHERE City = 'Panvel';
 
 --Protocol 4: Filter by age threshold
 SELECT Name,Age FROM Students WHERE Age>20;
+-- Day 2 | Mission: ORDER BY Master
+
+-- Protocol 5: Sort by Age - Youngest to Oldest
+SELECT Name, Age, City 
+FROM Students 
+ORDER BY Age ASC;
+
+-- Protocol 6: Sort by Marks - Highest First | Toppers List
+SELECT Name, Marks 
+FROM Students 
+ORDER BY Marks DESC;
+
+-- Protocol 7: Top 3 Oldest Students | LIMIT is your friend
+SELECT Name, Age 
+FROM Students 
+ORDER BY Age DESC 
+LIMIT 3;
+
+-- Protocol 8: Panvel Students sorted by Name | WHERE + ORDER BY
+SELECT Name, Age 
+FROM Students 
+WHERE City = 'Panvel' 
+ORDER BY Name ASC;
