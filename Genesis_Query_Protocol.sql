f2247f9531da6a6da223ddab17849f7ff61ed993
Tanvi-78 <dalvitanvi2008@gmail.com>
2026-06-16 18:10:53 +0530

    Add Protocol 9 SQL queries

--- Genesis_Query_Protocol.sql
+++ Genesis_Query_Protocol.sql
@@ -36,3 +36,59 @@ SELECT Name, Age
 FROM Students 
 WHERE City = 'Panvel' 
 ORDER BY Name ASC;
+-- Day 3 | 
+-- Protocol 9: SQL Student Marks Analyzer
+-- Task 1: Create Tables and Insert Data
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    roll_no INT
+);
+
+CREATE TABLE marks (
+    mark_id INT PRIMARY KEY,
+    student_id INT,
+    subject VARCHAR(30),
+    score INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+INSERT INTO students (student_id, name, roll_no) VALUES
+(1, 'Tanvi Dalvi', 101),
+(2, 'Rahul Sharma', 102),
+(3, 'Priya Patel', 103),
+(4, 'Amit Kumar', 104);
+
+INSERT INTO marks (mark_id, student_id, subject, score) VALUES
+(1, 1, 'SQL', 95), (2, 1, 'Python', 88), (3, 2, 'SQL', 76),
+(4, 2, 'Python', 91), (5, 3, 'SQL', 98), (6, 3, 'Python', 94),
+(7, 4, 'SQL', 65), (8, 4, 'Python', 70);
+
+-- Task 2: Find the College Topper with Subject and Score
+SELECT 
+    s.name AS student_name, 
+    m.subject, 
+    m.score 
+FROM students s 
+JOIN marks m ON s.student_id = m.student_id 
+WHERE m.score = (SELECT MAX(score) FROM marks);
+
+-- Task 3: Calculate Average Marks and Rank Students
+SELECT 
+    s.name AS student_name, 
+    ROUND(AVG(m.score), 2) AS average_marks 
+FROM students s 
+JOIN marks m ON s.student_id = m.student_id 
+GROUP BY s.student_id, s.name 
+ORDER BY average_marks DESC;
+
+-- Task 4: Filter Students with SQL Score Greater Than 80
+SELECT 
+    s.name AS student_name, 
+    m.score AS sql_marks
+FROM students s 
+JOIN marks m ON s.student_id = m.student_id 
+WHERE m.subject = 'SQL' AND m.score > 80
+ORDER BY sql_marks DESC;
+
+-
